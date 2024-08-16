e00042a760f00f018b0c976f1208797e3e83eb5b
전라남도교육지원청 <kimsd1983@naver.com>
2024-08-16 13:55:07 +0900

    24.08.15.
    새 페이지

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("streamlit 원데이 클래스")
-st.info("안녕하세요.")
-st.error("안녕하세요.")
-st.success("안녕하세요.")
+st.title("전라남도교육지원청")
+col1, col2 = st.columns(2)
+with col1:
+    st.image("https://preview.redd.it/jpiwcf6r1rx31.jpg?width=479&format=pjpg&auto=webp&s=c9973c18f74c40ea364144611ec76001c167708b")
+with col2:
+    st.link_button("티스토리", "https://celbeing.tistory.com/")
+    st.link_button("백준", "https://www.acmicpc.net/user/kimsd1983")
+    st.link_button("atCoder", "https://atcoder.jp/users/kimsd1983")
---
+++ pages/acmicpc.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title("백준 온라인 저지")
+user_name = st.text_input("백준핸들")
